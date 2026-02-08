d9c85b74e05e75d724c580aa22927bfaf3fa485f
Bryan Carlo Miguel <bryancarlomiguel@gmail.com>
2026-02-08 01:21:21 -0500

    Initial commit

---
+++ src/ShadcnBlazor/Metadata/ComponentMetadataAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace ShadcnBlazor.Metadata;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ComponentMetadataAttribute : Attribute
+{
+    public required string Name { get; set; }
+    public required string[] Dependencies { get; set; }
+    public required string[] Description { get; set; }
+}
