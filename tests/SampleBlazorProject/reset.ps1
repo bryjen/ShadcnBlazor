6e952fd3231e3a1da78066ffeef98d8e4e119b2c
Bryan Carlo Miguel <bryancarlomiguel@gmail.com>
2026-02-08 14:29:31 -0500

    adds some comments to the reset script

--- tests/SampleBlazorProject/reset.ps1
+++ tests/SampleBlazorProject/reset.ps1
@@ -1,1 +1,4 @@
-﻿git checkout abdbac3 -- tests/SampleBlazorProject && git clean -fd tests/SampleBlazorProject
+﻿# This script resets the SampleBlazorProject directory to a specific commit state.
+# Notably, one where no changes to the project files were made.
+# Allows for testing functionality for a "tracked" project without any actual changes.
+git checkout abdbac3 -- tests/SampleBlazorProject && git clean -fd tests/SampleBlazorProject
